b391acd8ba115145fc678c59773502863b0cef27
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 05:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function stringChop(str, size) {
   if(!str)
 	  return [];
 	
-	let result=[];
-	for(let i=0; i<str.length; i = i+size)
-		{
-			result.push(str.subString(i,i+size));
-		}
-		return result;
+let result=[];
+for(let i=0; i<str.length; i = i+size)
+{
+	result.push(str.subString(i,i+size));
+}
+return result;
 }
 
 // Do not change the code below
